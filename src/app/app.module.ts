77c27bb45baa93e8f02b6ba15f945a1fb5a53a4a
Vuk Topalovic <63306506+vukkt@users.noreply.github.com>
2023-10-21 07:16:21 +0200

    Your commit message here

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    LandingComponent
+    LandingComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
